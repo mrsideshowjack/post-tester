8426204820054802ea01f6487d207174fe4d977e
Jack Mason <jack.mason1@ibm.com>
2021-12-14 11:52:33 +0000

    test

--- netlify/functions/post-tester/post-tester.js
+++ netlify/functions/post-tester/post-tester.js
@@ -1,16 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const querystring = require("querystring");
 const handler = async (event) => {
 
     if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const params = querystring.parse(event.body);
-
   return {
     statusCode: 200,
-    body: params,
+    body: JSON.stringify(event.body),
   };
 
 }
